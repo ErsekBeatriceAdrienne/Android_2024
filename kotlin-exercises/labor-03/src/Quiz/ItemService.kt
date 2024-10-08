fe9b7c08acd8952c38f64a8c9fd46a816bfa2c12
Bea <99785877+ErsekBeatriceAdrienne@users.noreply.github.com>
2024-10-08 10:51:00 +0300

    labor 3 done

--- kotlin-exercises/labor-03/src/Quiz/ItemService.kt
+++ kotlin-exercises/labor-03/src/Quiz/ItemService.kt
@@ -3,5 +3,23 @@ package src.Quiz
 class ItemService ( val repository : ItemRepository )
 {
     /* SELECTS RANDOM ITEM FROM THE LIST*/
-    fun selectRandomItems( nr : Int ) : List < Item > = repository.items.shuffled().take( nr )
+    fun selectRandomItems( nr : Int ) : List < Item >
+    {
+        var random = mutableListOf < Item > ()
+        var randomQuestions = mutableListOf <String> ()
+
+        while( random.size < nr)
+        {
+            val r = repository.randomItem()
+            val q = r.question
+
+            if (!randomQuestions.contains(q))
+            {
+                random.add(r)
+                randomQuestions.add(q)
+            }
+        }
+
+        return random
+    }
 }
