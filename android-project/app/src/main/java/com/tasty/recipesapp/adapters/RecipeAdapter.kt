d0c29e49989a7a27823a8353f6e2db89408088f4
Bea <99785877+ErsekBeatriceAdrienne@users.noreply.github.com>
2024-11-20 19:18:36 +0200

    labor 8 recipe fragment details from database

--- android-project/app/src/main/java/com/tasty/recipesapp/adapters/RecipeAdapter.kt
+++ android-project/app/src/main/java/com/tasty/recipesapp/adapters/RecipeAdapter.kt
@@ -1,27 +1,19 @@
 package com.tasty.recipesapp.adapters
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.findFragment
-import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+import android.widget.Button
 import androidx.recyclerview.widget.RecyclerView
 import com.tasty.recipesapp.R
 import com.tasty.recipesapp.databinding.ItemRecipeBinding
 import com.tasty.recipesapp.models.recipe.RecipeModel
-import com.tasty.recipesapp.repository.LocalRepository
-import com.tasty.recipesapp.repository.RecipeRepository
-import com.tasty.recipesapp.ui.recipe.RecipeFragmentDirections
-import com.tasty.recipesapp.ui.favorites.FavoritesFragmentDirections
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
-class RecipeAdapter(
-    private var recipes: MutableList<RecipeModel>
-) : RecyclerView.Adapter<RecipeAdapter.RecipesViewHolder>()
+class RecipeAdapter(private var recipes: MutableList<RecipeModel>)
+    : RecyclerView.Adapter<RecipeAdapter.RecipesViewHolder>()
 {
+    // Define a listener for long clicks
+    var onRecipeLongClickListener: ((RecipeModel) -> Unit)? = null
+    var onRecipeClickListener: ((RecipeModel) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipesViewHolder
     {
@@ -31,22 +23,35 @@ class RecipeAdapter(
 
     override fun onBindViewHolder(holder: RecipesViewHolder, position: Int)
     {
-        holder.bind(recipes[position])
+        val recipe = recipes[position]
+        holder.bind(recipe)
+
+        // Set the listener for the "More Details" button
+        holder.itemView.findViewById<Button>(R.id.buttonMoreDetails).setOnClickListener {
+            onRecipeClickListener?.invoke(recipe)
+        }
+    }
+
+    fun updateRecipes(newRecipes: MutableList<RecipeModel>) {
+        recipes.clear()
+        recipes.addAll(newRecipes)
+        notifyDataSetChanged()
     }
 
     override fun getItemCount(): Int = recipes.size
 
-    inner class RecipesViewHolder(private val binding: ItemRecipeBinding) :
-        RecyclerView.ViewHolder(binding.root) {
+    inner class RecipesViewHolder(private val binding: ItemRecipeBinding) : RecyclerView.ViewHolder(binding.root) {
+        init {
+            // Handle long click to show delete confirmation dialog
+            binding.root.setOnLongClickListener {
+                onRecipeLongClickListener?.invoke(recipes[adapterPosition])  // Trigger the long click callback
+                true // Indicate that the long click was handled
+            }
+        }
+
         fun bind(recipe: RecipeModel) {
             binding.recipe = recipe
             binding.executePendingBindings()
         }
-    }
-
-    // Update the recipe list
-    fun updateRecipes(newRecipes: List<RecipeModel>) {
-        recipes = newRecipes.toMutableList()
-        notifyDataSetChanged()
     }
 }
--- android-project/app/src/main/java/com/tasty/recipesapp/ui/recipe/RecipeFragment.kt
+++ android-project/app/src/main/java/com/tasty/recipesapp/ui/recipe/RecipeFragment.kt
@@ -1,25 +1,20 @@
 package com.tasty.recipesapp.ui.recipe
 
 import android.os.Bundle
-import android.os.LocaleList
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.tasty.recipesapp.R
-import com.tasty.recipesapp.RecipeApp
-import com.tasty.recipesapp.activities.MainActivity
 import com.tasty.recipesapp.adapters.RecipeAdapter
 import com.tasty.recipesapp.database.RecipeDatabase
 import com.tasty.recipesapp.databinding.FragmentRecipeBinding
@@ -27,10 +22,7 @@ import com.tasty.recipesapp.models.recipe.RecipeModel
 import com.tasty.recipesapp.models.recipe.RecipeViewModel
 import com.tasty.recipesapp.models.recipe.RecipeViewModelFactory
 import com.tasty.recipesapp.repository.LocalRepository
-import com.tasty.recipesapp.repository.RecipeRepository
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class RecipeFragment : Fragment() {
     private lateinit var binding: FragmentRecipeBinding
@@ -53,7 +45,6 @@ class RecipeFragment : Fragment() {
         return binding.root
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -61,13 +52,23 @@ class RecipeFragment : Fragment() {
         binding.recyclerViewRecipes.layoutManager = LinearLayoutManager(requireContext())
 
         // Initialize the adapter with an empty list
+
         recipeAdapter = RecipeAdapter(mutableListOf())
+        recipeAdapter.onRecipeLongClickListener = { recipe ->
+            showDeleteConfirmationDialog(recipe)
+        }
+        // Set click listener for "More Details" button in each recipe card
+        recipeAdapter.onRecipeClickListener = { recipe ->
+            val action = RecipeFragmentDirections.actionRecipeFragmentToRecipeDetailFragment(recipe.recipeID.toString())
+            findNavController().navigate(action)
+        }
+
         binding.recyclerViewRecipes.adapter = recipeAdapter
 
         // Observe the ViewModel for updates
         viewModel.recipes.observe(viewLifecycleOwner) { recipes ->
             allRecipes.addAll(recipes)
-            recipeAdapter.updateRecipes(recipes)
+            recipeAdapter.updateRecipes(recipes.toMutableList())
             updateUIBasedOnRecipes(recipes)
         }
 
@@ -75,23 +76,32 @@ class RecipeFragment : Fragment() {
         searchRecipe()
     }
 
-    private fun showDeleteConfirmationDialog(recipe: RecipeModel)
-    {
+    private fun showDeleteConfirmationDialog(recipe: RecipeModel) {
         val builder = AlertDialog.Builder(requireContext())
             .setTitle("Delete Recipe")
             .setMessage("Are you sure you want to delete the recipe '${recipe.name}'?")
             .setIcon(R.drawable.delete)
             .setPositiveButton("Yes") { _, _ ->
                 viewLifecycleOwner.lifecycleScope.launch {
+                    // Perform deletion
                     viewModel.deleteRecipe(recipe)
-                }}
+
+                    // Remove from the adapter's list and update the UI
+                    allRecipes.remove(recipe)
+                    recipeAdapter.updateRecipes(allRecipes)
+
+                    // Optionally, show a snackbar or toast confirming the deletion
+                    Toast.makeText(requireContext(), "Recipe deleted", Toast.LENGTH_SHORT).show()
+                }
+            }
             .setNegativeButton("No") { dialog, _ -> dialog.dismiss() }
             .setCancelable(true) // Allow dismissing the dialog by tapping outside
 
-        // Create and customize the AlertDialog
+        // Create and show the AlertDialog
         val dialog = builder.create()
         dialog.show()
     }
+
 
     private fun searchRecipe() {
         searchEditText = binding.editTextSearch
